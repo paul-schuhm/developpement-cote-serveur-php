19e674bfce902988e45e4cede00b7d5a981c4bf8
p schuhmacher <contact@pschuhmacher.com>
2025-03-24 11:52:47 +0100

    met a jour la demo

--- demos/live/2025-03-24/cms/index.php
+++ demos/live/2025-03-24/cms/index.php
@@ -13,6 +13,9 @@ if (isset($_POST['publish'])) {
     fclose($file);
 }
 
+//2. Récupérer les messages publiés
+$messages = file('database.txt', FILE_IGNORE_NEW_LINES);
+
 ?>
 
 
@@ -29,8 +32,15 @@ if (isset($_POST['publish'])) {
 
     <h1>Blog</h1>
 
+    <!-- On affiche les messages précédemment publiés -->
+    <ul>
+        <?php foreach ($messages as $message) : ?>
+            <li><?php echo htmlentities($message); ?></li>
+        <?php endforeach; ?>
+    </ul>
+
     <form action="" method="POST">
-        <label for="message">Laisser un message</label>
+        <label for="message">Ajouter un message</label>
         <input type="text" name="message" id="message">
         <input type="submit" name="publish" value="Publier">
     </form>
